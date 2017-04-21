2d6b29030790185bdb81aaf329eead914cbf7f55
artunggal <a.r.tunggal@gmail.com>
2017-04-21 03:12:13 -0700

    Add files via upload

---
+++ connect-db.php
@@ -0,0 +1,24 @@
+<?php
+
+function Connect() {
+	$servername = 'localhost';
+	$username = ******;
+	$password = *******;
+	$dbname = *******;
+
+	// Create connection
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+	else {
+		echo "Connection successful";
+	}
+
+	return $conn;
+}
+
+?>
